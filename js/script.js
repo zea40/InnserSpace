6fc142c3cb8f5df80298e204594d411b66db8fe8
zea40 <kayyisanauraelmamazea@gmail.com>
2026-01-19 20:54:12 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-function cariAyat() {
+function cariayat() {
   const text = document.getElementById("curhat").value.toLowerCase();
 
   // ambil semua ayat yang keyword-nya cocok
-  const cocok = dataAyat.filter(item =>
+  const cocok = dataayat.filter(item =>
     text.includes(item.keyword)
   );
 
@@ -32,7 +32,7 @@ function cariAyat() {
   if (arti) hasil += "\n\n" + arti;
   if (surat) hasil += "\n(" + surat + ")";
 
-  document.getElementById("hasilAyat").innerText = hasil;
+  document.getElementById("hasilayat").innerText = hasil;
   document.getElementById("popup").classList.add("show");
 }
 
